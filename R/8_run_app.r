0bd30da6a72c4a1266c275bb5b69090ea131a109
Nick Spyrison <spyrison@gmail.com>
2021-09-24 14:43:25 +1000

    - missed run_app() - Installation and run_app() on readme!

---
+++ R/8_run_app.r
@@ -0,0 +1,45 @@
+#' Runs a shiny app demonstrating manual tours
+#' 
+#' Runs a local shiny app that demonstrates manual tour and comparable 
+#' traditional techniques for static projections of multivariate data sets.
+#' 
+#' @param app_nm Name of the shiny app to run. Expects "cheem_initial".
+#' @param ... Other arguments passed into `shiny::runApp()`. 
+#' Such as display.mode = "showcase".
+#' @return Runs a locally hosted shiny app.
+#' @export
+#' @examples
+#' \dontrun{
+#' run_app("cheem_initial")
+#' run_app(app_nm = "cheem_initial", display.mode = "showcase")}
+# For adjusting or adding more apps it may be useful to read: 
+# https://deanattali.com/2015/04/21/r-package-shiny-app/
+run_app <- function(app_nm = 'cheem_initial', ...) {
+  ### Additional dependencies for shiny app.
+  shiny_depends <-  c("shinythemes", "shinycssloaders", "plotly", "DT")
+  pkgs_needed <- !sapply(shiny_depends, requireNamespace)
+  if(max(pkgs_needed) == TRUE) { ## Needs at least 1 package.
+    .inst_code <-
+      paste0("install.packages('", shiny_depends[pkgs_needed], "'); ", collapse = "")
+    stop(
+      paste0("app_nm = '", app_nm, "' has additional dependancies that were not found. ",
+             "Please run the following code before trying again.  \n", .inst_code),
+      call. = FALSE)
+  }
+  
+  ## Locate all the shiny app names that exist
+  valid_app_nms <- list.files(system.file("shiny_apps", package = "cheem"))
+  valid_msg <- paste0(
+    "Valid app_nm : '", paste(valid_app_nms, collapse = "', '"), "'")
+  
+  ## If an invalid app_nm is given, throw an error
+  if(!nzchar(app_nm) || !(app_nm %in% valid_app_nms)) {
+    stop(paste0('App_nm was missing or invalid. \n', valid_msg,
+                "\n are the only valid app_nm at the for this version."),
+         call. = FALSE)
+  }
+  
+  ## Find and launch the app
+  app_dir <- system.file("shiny_apps", app_nm, package = "cheem")
+  shiny::runApp(app_dir, ...)
+}
--- README.md
+++ README.md
@@ -8,6 +8,14 @@ _Local explanations_ are an approximation of instance(observation) level variabl
 
 `{cheem}` extracts the local explanation of every observation in a dataset, given a model. Given a model, extract the local explanation of every observation in a data set. View the data- and explanation-spaces side-by-side in an interactive shiny application. Further explored a selected point against a comparison by using its explanation as a 1D projection basis. The structure of explanation projection is then explored by a radial tour.
 
+## Getting started
+
+```
+## Install development version from GitHub
+remotes::install_github("nspyrison/cheem", dependencies = TRUE)
+## Run the {cheem} app including 3 preprocessed datasets
+cheem::run_app()
+```
 ### Original application
 
 We started by looking at the model-agnostic local explantion _SHAP_ as applied to random forests. We made this choice our of concern for runtime (`{treeshap}` uses an an alternative algorithm with reduced computational complexity and thus achieves much faster run time in the _preprocessing_ step). The namesake, Cheem, stemmed from this tree-based approach. The [Cheem](https://tardis.fandom.com/wiki/Tree_of_Cheem) are a fictional race of tree-based humanoids for consistency with the Dr. who/Dr. why theme of the {DALEX} ecosystem.
